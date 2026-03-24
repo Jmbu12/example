b1cfc8a293f7454aa0d30a3d71de10173e3bf982
Jambuu <lebdaandra12@webmail.umm.ac.id>
2026-03-24 23:00:28 +0700

    up benar

--- src/Tugas_2/Main.java
+++ src/Tugas_2/Main.java
@@ -1,4 +1,55 @@
 package Tugas_2;
 
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n=========================================");
+            System.out.println("Welcome to Smart City Management System : ");
+            System.out.println("1. Add New Building.");
+            System.out.println("2. View All Building");
+            System.out.println("3. Exit.");
+            System.out.println("=========================================");
+            System.out.print("Pilih menu :  ");
+            int pilih = input.nextInt();
+            input.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    System.out.println("\n=========================================");
+                    System.out.print("Name Building : ");
+                    String building = input.nextLine();
+
+                    System.out.print("Addres : ");
+                    String Addres = input.nextLine();
+
+                    System.out.print("Floor : ");
+                    String Floor = input.nextLine();
+
+                    System.out.println("\n=========================================");
+                    System.out.println("Building data");
+                    System.out.println("Name Building: " + building);
+                    System.out.println("Adrees: " + Addres);
+                    System.out.println("Floor: " + Floor);
+                    System.out.println("=========================================");
+                    System.out.println("sucsess to add");
+
+                    break;
+
+                case 2:
+                    System.out.println("coming soon");
+                    break;
+
+                case 3:
+                    System.out.println("the end");
+                    break;
+
+                default:
+                    System.out.println("invalid option");
+            }
+        }
+    }
 }
